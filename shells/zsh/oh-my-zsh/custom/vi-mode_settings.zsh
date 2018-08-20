3d301e439245aaa9a6a3cc4ca65d20735766a7e7
Rahul Mohandas <rahul0705@gmail.com>
2018-08-19 23:32:04 -0400

    master fix(zsh): fixed vi-mode shift tab and home and end work

--- shells/zsh/oh-my-zsh/custom/vi-mode_settings.zsh
+++ shells/zsh/oh-my-zsh/custom/vi-mode_settings.zsh
@@ -1,3 +1,6 @@
+#  allow shift-tab still to reverse completion (standard behaviour)
+bindkey '^[[Z' reverse-menu-complete
+
 # Better searching in command mode
 bindkey -M vicmd '?' history-incremental-search-backward
 bindkey -M vicmd '/' history-incremental-search-forward
@@ -7,3 +10,6 @@ bindkey "^[OA" up-line-or-beginning-search
 bindkey "^[OB" down-line-or-beginning-search
 bindkey -M vicmd "k" up-line-or-beginning-search
 bindkey -M vicmd "j" down-line-or-beginning-search
+
+bindkey "${terminfo[khome]}" beginning-of-line
+bindkey "${terminfo[kend]}" end-of-line
